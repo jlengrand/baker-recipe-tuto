6cc805be60d33205d7648198c9f8609fc57bfabf
julien Lengrand-Lambert <julien@lengrand.fr>
2017-05-04 08:43:03 +0200

    Creates baker recipe project

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "baker-recipe-tuto"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+libraryDependencies += "com.ing" % "baker_2.11" % "0.2.30"
---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by jll on 5/4/2017.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
