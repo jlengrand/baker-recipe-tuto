54615a5eb640dd4af5ca22e6f35d5dd09b6cd8f6
julien Lengrand-Lambert <julien@lengrand.fr>
2017-05-05 08:32:07 +0200

    Runs simple recipe

--- src/main/java/erwtensoep/ErwtenSoepRecipeUtils.java
+++ src/main/java/erwtensoep/ErwtenSoepRecipeUtils.java
@@ -1,15 +1,21 @@
-package erwtensoep.recipes;
+package erwtensoep;
 
+import com.google.common.collect.ImmutableList;
+import com.ing.baker.core.Interaction;
 import com.ing.baker.java_api.JInteractionDescriptor;
 import com.ing.baker.java_api.JRecipe;
 import erwtensoep.events.BoodschappenGedaan;
 import erwtensoep.events.KitchenToolsReady;
 import erwtensoep.interactions.SoupPreheating;
 
+import java.util.List;
+
 /**
+ * Note : Those are methods that are created in Java to save some time
+ * The class should be removed and replaced by scala implementations instead
  * Created by jll on 5/4/2017.
  */
-public final class ErwtenSoepRecipeCreator {
+public final class ErwtenSoepRecipeUtils {
 
     public JRecipe createRecipe(){
         return new JRecipe("ErwtenSoepRecipe")
@@ -22,4 +28,9 @@ public final class ErwtenSoepRecipeCreator {
                 ;
     }
 
+    public List<Interaction> getInteractions(){
+        return ImmutableList.<Interaction>of(
+                new SoupPreheating()
+        );
+    }
 }
--- src/main/scala/ErwtenSoepCooker.scala
+++ src/main/scala/ErwtenSoepCooker.scala
@@ -1,7 +1,12 @@
-import com.ing.baker.java_api.JCompiledRecipe
-import erwtensoep.events.KitchenToolsReady
+import java.util.UUID
+
+import com.google.common.collect.ImmutableList
+import com.ing.baker.java_api.{JBaker, JCompiledRecipe}
+import erwtensoep.ErwtenSoepRecipeUtils
+import erwtensoep.events.{BoodschappenGedaan, KitchenToolsReady}
 import erwtensoep.ingredients.SplitErwten
-import erwtensoep.recipes.{ErwtenSoepRecipe, ErwtenSoepRecipeCreator}
+import erwtensoep.interactions.SoupPreheating
+import erwtensoep.recipes.ErwtenSoepRecipe
 
 /**
   * Created by jll on 5/4/2017.
@@ -10,10 +15,32 @@ object ErwtenSoepCooker {
   def main(args: Array[String]): Unit = {
 
     // Let's create a recipe and validate it
-    val recipeCreator = new ErwtenSoepRecipeCreator
+    val recipeCreator = new ErwtenSoepRecipeUtils
     val recipe = recipeCreator.createRecipe()
     val compiledRecipe = recipe.compileRecipe
 
     println(compiledRecipe.getRecipeVisualization())
+
+    //Actual implementation
+    val baker = new JBaker(
+      recipe,
+      recipeCreator.getInteractions
+    )
+
+    val id = UUID.randomUUID();
+    baker.bake(id)
+
+    //Firing events
+    baker.processEvent(id, new KitchenToolsReady())
+    baker.processEvent(id, new BoodschappenGedaan())
+
+    // Recipe should be ran now
+    val ingredients = baker.getIngredients(id);
+    val events = baker.getEvents(id);
+
+    println(ingredients)
+    println(events)
+
+    baker.shutdown()
   }
 }
